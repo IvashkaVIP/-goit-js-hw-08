40a2a7599c0fcf026b20ac82f8f24f2825be0b11
IvashkaVIP <ivashka.co@gmail.com>
2023-05-05 23:54:50 +0300

    task-02 end

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,17 @@
 import Vimeo from '@vimeo/player';
+import lodash from 'lodash.throttle'
 
 //console.log(Vimeo);
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
-const playTime = localStorage.getItem('videoplayer-current-time');
 
-if (playTime) {
-  player.setCurrentTime(playTime);
-}
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', lodash(onPlay,1000));
 
 function onPlay(data) {
   {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
+        localStorage.setItem('videoplayer-current-time', data.seconds);
+        // console.log(localStorage.getItem('videoplayer-current-time'));
   }
-}
