10992c6cd8dcc4f1d71063713992401c2c615156
IvashkaVIP <ivashka.co@gmail.com>
2023-05-06 16:33:39 +0300

    task-03 осталось добавить объект

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,20 +1,36 @@
+import throttle from 'lodash.throttle';
+
 const refs = {
-    form: document.querySelector('.feedback-form'),
-    textarea : document.querySelector('textarea'),
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('textarea'),
 };
+
+const STORAGE_KEY = 'feedback-form-state';
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', ontextareaInput);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateTextareaInput();
 
 function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log('отправляем форму');
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
 
-};
+function onTextareaInput(evt) {
+  const message = evt.target.value;
 
-function ontextareaInput(evt) {
-    const message = evt.currentTarget.value;
+  localStorage.setItem(STORAGE_KEY, message);
 
-    localStorage.setItem('feedback-form-state', message);
+  // console.log(message);
+}
 
-    // console.log(message);
-    
-};
+function populateTextareaInput() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    refs.textarea.value = savedMessage;
+  }
+}
