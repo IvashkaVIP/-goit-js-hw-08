e103a26c5e11614aac9873cdf90cfb27dcf6fe51
IvashkaVIP <ivashka.co@gmail.com>
2023-05-06 22:01:25 +0300

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,45 +3,39 @@ import throttle from 'lodash.throttle';
 const refs = {
   form: document.querySelector('.feedback-form'),
   textarea: document.querySelector('textarea'),
-  emailarea: document.querySelector('input')
+  emailarea: document.querySelector('input'),
 };
 
 const STORAGE_KEY = 'feedback-form-state';
 let formData = {};
 
-refs.form.addEventListener('input',throttle(onFormInput,500))
+refs.form.elements.email.setAttribute('required', '');
+refs.form.elements.message.setAttribute('required', '');
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
 refs.form.addEventListener('submit', onFormSubmit);
-//refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
 
 populateTextareaInput();
 
 function onFormSubmit(evt) {
-    evt.preventDefault();
-    console.log('отправляем форму');
-    console.log(formData);
-    evt.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-    //formData = {};
-}
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-function populateTextareaInput() {
-    formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    //console.log('saveData: ' , savedData);
-
-   if (formData) {
-       refs.emailarea.value = formData.email || '';
-       refs.textarea.value = formData.message || '';
-    }
-        
-}
-///!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-function onFormInput(evt) {
-  
+  evt.preventDefault();
+  console.log('отправляем форму');
   console.log(formData);
-  formData[evt.target.name] = evt.target.value;
-  console.log(`formData[${evt.target.name}]: = `, formData[evt.target.name]);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {};
+}
 
+function populateTextareaInput() {
+  formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+  if (formData) {
+    refs.emailarea.value = formData.email || '';
+    refs.textarea.value = formData.message || '';
+  }
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  //console.log(formData);
 }
