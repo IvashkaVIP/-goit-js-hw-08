d5101fe79da78306b68240ffe9f0ea5f2f9a5ad5
IvashkaVIP <86344094+IvashkaVIP@users.noreply.github.com>
2023-05-06 13:34:20 +0000

    Deploying to gh-pages from @ IvashkaVIP/goit-js-hw-08@10992c6cd8dcc4f1d71063713992401c2c615156 🚀

--- 03-feedback.2c794f89.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector(".feedback-form"),textarea:document.querySelector("textarea")};e.form.addEventListener("submit",(function(e){})),e.textarea.addEventListener("input",(function(e){var t=e.currentTarget.value;localStorage.setItem("feedback-form-state",t)}))}();
-//# sourceMappingURL=03-feedback.2c794f89.js.map
--- 03-feedback.f8b0a363.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".feedback-form"),textarea:document.querySelector("textarea")};e.form.addEventListener("submit",(function(e){})),e.textarea.addEventListener("input",(function(e){const t=e.currentTarget.value;localStorage.setItem("feedback-form-state",t)}));
-//# sourceMappingURL=03-feedback.f8b0a363.js.map
